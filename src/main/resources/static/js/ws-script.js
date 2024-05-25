a696b6cdabf41502392c6a9e259ca3b28e16a09e
Tomasz Jaworski <tomasz.jaworski@adtran.com>
2024-05-25 18:27:15 +0200

    JS websocket refactor

--- src/main/resources/static/js/ws-script.js
+++ src/main/resources/static/js/ws-script.js
@@ -4,36 +4,22 @@ const path = '/echo';
 // Create a WebSocket instance
 const socket = new WebSocket(`ws://${hostname}:${port}${path}`);
 
-// Define event listeners for WebSocket
-socket.addEventListener('open', () => {
-    console.log('Connected to WebSocket server');
-});
-
-socket.addEventListener('message', (event) => {
+socket.onmessage = function (event) {
+    console.log("WebSocket message received: ", event.data);
     const messagesContainer = document.getElementById('messages');
     const message = document.createElement('div');
     message.textContent = event.data;
     messagesContainer.innerHTML = event.data;
-});
+};
 
-socket.addEventListener('close', () => {
-    console.log('WebSocket connection closed');
-});
-
-socket.onerror = function(error) {
+socket.onerror = function (error) {
     console.error("WebSocket error: ", error);
 };
 
-// Function to handle WebSocket connection opened
-socket.onopen = function(event) {
+socket.onopen = function (event) {
     console.log("WebSocket connection opened.");
 };
 
-// Handle form submission
-document.getElementById('message-form').addEventListener('submit', (event) => {
-    event.preventDefault();
-    const input = document.getElementById('message-input');
-    const message = input.value;
-    socket.send(message);
-    input.value = '';
-});
+socket.onclose = function (event) {
+    console.log("WebSocket connection closed.");
+};
