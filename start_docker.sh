c897370516931bd29d06471572d256c92ef24ea2
Tomek Jaworski <tj.toja@gmail.com>
2024-06-01 12:07:02 +0200

    refactor script

--- start_docker.sh
+++ start_docker.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-# Check if docker-compose is installed
-if ! command -v docker-compose &> /dev/null
+# Check if docker compose is installed
+docker compose version
+
+if [ $? -ne 0 ];
 then
-    echo "docker-compose could not be found, please install it."
+    echo "docker compose could not be found, please install it."
     exit 1
 fi
 
-# Run docker-compose build
+# Run docker compose build
 echo "Building Docker images..."
 docker compose build
 
 if [ $? -ne 0 ]; then
-    echo "Docker-compose build failed."
+    echo "Docker compose build failed."
     exit 1
 fi
 
-# Run docker-compose up
+# Run docker compose up
 echo "Starting Docker containers..."
 docker compose up
 
 if [ $? -ne 0 ]; then
-    echo "Docker-compose up failed."
+    echo "Docker compose up failed."
     exit 1
 fi
