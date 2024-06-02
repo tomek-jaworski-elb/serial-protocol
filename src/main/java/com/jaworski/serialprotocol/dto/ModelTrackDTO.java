c3f54de2927f1a003f178ccde735028c365abcf1
Tomek Jaworski <tj.toja@gmail.com>
2024-06-02 18:48:33 +0200

    Implement Builder for ModelTrackDTO

--- src/main/java/com/jaworski/serialprotocol/dto/ModelTrackDTO.java
+++ src/main/java/com/jaworski/serialprotocol/dto/ModelTrackDTO.java
@@ -1,14 +1,12 @@
 package com.jaworski.serialprotocol.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @EqualsAndHashCode
 @AllArgsConstructor
 @ToString
+@Builder
 public class ModelTrackDTO {
 
     private int modelName;
@@ -18,4 +16,9 @@ public class ModelTrackDTO {
     private double heading;
     private double rudder;
     private double gpsQuality;
+    private double engine;
+    private double tugBowForce;
+    private double tugBowDirection;
+    private double tugSternForce;
+    private double tugSternDirection;
 }
--- src/main/java/com/jaworski/serialprotocol/service/utils/MessageTranslator.java
+++ src/main/java/com/jaworski/serialprotocol/service/utils/MessageTranslator.java
@@ -8,10 +8,8 @@ import org.apache.logging.log4j.Logger;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
+import java.math.RoundingMode;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 public class MessageTranslator {
@@ -42,37 +40,6 @@ public class MessageTranslator {
         }
     }
 
-    public static String fromBinary(byte[] delimitedMessage) {
-        ModelTrackDTO dto = null;
-        try {
-            int modelId = getModelId(delimitedMessage);
-            Double speed = getSpeed(delimitedMessage);
-            Double heading = getHeading(delimitedMessage);
-            Double rudder = getRudder(delimitedMessage);
-            Double gpsQuality = getGPSQuality(delimitedMessage);
-            dto = new ModelTrackDTO(modelId, 0.0f, 0.0f, speed, heading, rudder, gpsQuality);
-            LOG.info("Received data {}", dto);
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.writeValueAsString(dto);
-            return objectMapper.writeValueAsString(dto);
-        } catch (JsonProcessingException | IllegalArgumentException | NullPointerException |
-                 ArrayIndexOutOfBoundsException e) {
-            LOG.error("Error while serializing dto {}", dto, e);
-            return "";
-        }
-    }
-
-    private static float bytesToFloat(byte[] bytes) {
-        if (bytes.length != 4) {
-            throw new IllegalArgumentException("Byte array must be exactly 4 bytes long for a float.");
-        }
-
-        // Use ByteBuffer to wrap the byte array
-        ByteBuffer buffer = ByteBuffer.wrap(bytes);
-
-        return Float.intBitsToFloat(buffer.getInt());
-    }
-
     public static Double getSpeed(byte[] message) {
         if (message == null || message.length != MESSAGE_LENGTH) {
             return null;
@@ -100,35 +67,46 @@ public class MessageTranslator {
         }
     }
 
-    public static long binaryStringToLong(String binaryString) {
-        // Validate the binary string
-        if (binaryString == null || binaryString.isEmpty()) {
-            throw new NumberFormatException("Binary string is null or empty");
+    public static Double getEngine(byte[] message) {
+        if (message == null || message.length != MESSAGE_LENGTH) {
+            return null;
+        } else {
+            byte speed = message[7];
+            return (double) speed;
         }
-
-        // Parse the binary string as a long integer
-        return Long.parseLong(binaryString, 2);
     }
 
-    public static String byteToBinaryString(byte b) {
-        // Convert the byte to a binary string directly
-        StringBuilder binaryString = new StringBuilder(8);
-        for (int i = 7; i >= 0; i--) {
-            binaryString.append((b & (1 << i)) >> i);
+    public static Double getTugBowForce(byte[] message) {
+        if (message == null || message.length != MESSAGE_LENGTH) {
+            return null;
         }
-        return binaryString.toString();
+        byte b = message[6];
+        return b / 10d;
     }
 
-    public static List<Byte> byteArrayToList(byte[] byteArray) {
-        // Create an ArrayList with the same size as the byteArray
-        List<Byte> byteList = new ArrayList<>(byteArray.length);
-        // Add each byte to the ArrayList
-        for (byte b : byteArray) {
-            byteList.add(b);
+    public static Double getTugSternForce(byte[] message) {
+        if (message == null || message.length != MESSAGE_LENGTH) {
+            return null;
         }
+        byte b = message[8];
+        return b / 10d;
+    }
 
-        // Convert Byte array to List
-        return byteList;
+    public static Double getTugBowDirection(byte[] message) {
+        if (message == null || message.length != MESSAGE_LENGTH) {
+            return null;
+        } else {
+            int direction = (message[4] & 0xFF) << 8 | (message[5] & 0xFF);
+            return direction / 100d;
+        }
+    }
+    public static Double getTugSternDirection(byte[] message) {
+        if (message == null || message.length != MESSAGE_LENGTH) {
+            return null;
+        } else {
+            int direction = (message[9] & 0xFF) << 8 | (message[10] & 0xFF);
+            return direction / 100d;
+        }
     }
 
     public static Double getRudder(byte[] message) {
@@ -140,8 +118,20 @@ public class MessageTranslator {
     }
 
     public static ModelTrackDTO getDTO(byte[] message) {
-        return new ModelTrackDTO(getModelId(message), getPositionX(message), getPositionY(message),
-                getSpeed(message), getHeading(message), getRudder(message), getGPSQuality(message));
+        return ModelTrackDTO.builder()
+                .modelName(getModelId(message))
+                .positionX(getPositionX(message))
+                .positionY(getPositionY(message))
+                .speed(getSpeed(message))
+                .heading(getHeading(message))
+                .rudder(getRudder(message))
+                .gpsQuality(getGPSQuality(message))
+                .engine(getEngine(message))
+                .tugBowForce(getTugBowForce(message))
+                .tugBowDirection(getTugBowDirection(message))
+                .tugSternForce(getTugSternForce(message))
+                .tugSternDirection(getTugSternDirection(message))
+                .build();
     }
 
     private static String getBinaryStringFromByte(byte b) {
@@ -194,16 +184,21 @@ public class MessageTranslator {
     }
 
     private static float getFloatFromBytes(byte[] bytes) {
+        bytes[0] = getNegateByte(bytes[0]);
+        bytes[1] = getNegateByte(bytes[1]);
+        bytes[2] = getNegateByte(bytes[2]);
+        bytes[3] = getNegateByte(bytes[3]);
         String binaryMessageNegate = getBinaryStringFromByte(bytes[0]) + getBinaryStringFromByte(bytes[1]) +
                 getBinaryStringFromByte(bytes[2]) + getBinaryStringFromByte(bytes[3]);
-        String mantysa = binaryMessageNegate.substring(1, 9);
-        String cecha = binaryMessageNegate.substring(9, binaryMessageNegate.length());
+        binaryMessageNegate = new StringBuilder(binaryMessageNegate).reverse().toString();
+        BigDecimal znak = BigDecimal.valueOf(-1).pow(Integer.parseInt(binaryMessageNegate.substring(0,1)));
+        String mantysa = binaryMessageNegate.substring(23, 31);
+        String cecha = binaryMessageNegate.substring(0,23);
         BigDecimal pow = BigDecimal.valueOf(2).pow(Integer.parseInt(mantysa, 2) - 127, MathContext.DECIMAL32);
-        int x = Integer.parseInt(cecha, 2);
-        String valueOf = String.valueOf(x);
-        valueOf = "0." + valueOf;
-        float aFloat = Float.parseFloat(valueOf);
-        BigDecimal result = BigDecimal.valueOf(aFloat).multiply(pow);
-        return result.floatValue();
+        int cechaInt = Integer.parseInt(cecha, 2);
+        String valueOf = String.valueOf(cechaInt);
+        float aFloat = Float.parseFloat("0." + valueOf);
+        BigDecimal result = BigDecimal.valueOf(aFloat).multiply(pow).multiply(znak);
+        return result.setScale(2, RoundingMode.UP).floatValue();
     }
 }
