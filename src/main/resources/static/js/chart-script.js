d8b8738a9dca612768cc44f4f41caa5b08e26124
Tomasz Jaworski <tomasz.jaworski@adtran.com>
2024-07-05 15:43:24 +0200

    JW changes in scale adaptation

--- src/main/resources/static/js/chart-script.js
+++ src/main/resources/static/js/chart-script.js
@@ -42,37 +42,48 @@ window.onload = function () {
                 case 1:
                     canvasName = "overlayCanvas1"
                     clearCanvas(canvasName);
-                    drawTriangle(canvasName, positionX, positionY, 4, angle, 'orange');
+                    //drawTriangle(canvasName, positionX, positionY, 8, angle, 'orange');
+                    drawShip(canvasName, positionX, positionY, 8, angle, 'orange');
+                    drawTriangle(canvasName, ( 0 + 60+4) * 3.6 , ( 0 + 507) * 3.6 , 12,    1, 'white'); // pozycja 0 x 0
+                    drawTriangle(canvasName, (77.07 + 60+4) * 3.6 , (97.25 + 507) * 3.6 , 12,    1, 'orange'); // SBM
+                    drawTriangle(canvasName, (378.03 + 60+4) * 3.6 , (191.8 + 507) * 3.6 , 12,    1, 'orange'); // FPSO
+                    drawTriangle(canvasName, (100 + 60+3) * 3.6 , (100 + 505) * 3.6 , 12,    1, 'white');
                     console.log("Drawing model with ID: " + modelId + " at position X: " + positionX + ", Y: " + positionY);
                     break;
                 case 2:
                     canvasName = "overlayCanvas2"
                     clearCanvas(canvasName);
-                    drawTriangle(canvasName, positionX, positionY, 4, angle, 'blue');
+                    //drawTriangle(canvasName, positionX, positionY, 8, angle, 'blue');
+                    drawShip(canvasName, positionX, positionY, 8, angle, 'blue');
                     console.log("Drawing model with ID: " + modelId + " at position X: " + positionX + ", Y: " + positionY);
                     break;
                 case 3:
                     canvasName = "overlayCanvas3"
                     clearCanvas(canvasName);
-                    drawTriangle(canvasName, positionX, positionY, 4, angle, 'green');
+                    //drawTriangle(canvasName, positionX, positionY, 8, angle, 'green');
+                    drawShip(canvasName, positionX, positionY, 8, angle, 'green');
                     console.log("Drawing model with ID: " + modelId + " at position X: " + positionX + ", Y: " + positionY);
                     break;
                 case 4:
                     canvasName = "overlayCanvas4"
                     clearCanvas(canvasName);
-                    drawTriangle(canvasName, positionX, positionY, 4, angle, 'purple');
+                    //drawTriangle(canvasName, positionX, positionY, 8, angle, 'purple');
+                    drawShip(canvasName, positionX, positionY, 8, angle, 'purple');
                     console.log("Drawing model with ID: " + modelId + " at position X: " + positionX + ", Y: " + positionY);
                     break;
                 case 5:
                     canvasName = "overlayCanvas5"
                     clearCanvas(canvasName);
-                    drawTriangle(canvasName, positionX, positionY, 4, angle, 'white');
+                    //drawTriangle(canvasName, positionX, positionY, 8, angle, 'white');
+                    drawShip(canvasName, positionX, positionY, 8, angle, 'white');
                     console.log("Drawing model with ID: " + modelId + " at position X: " + positionX + ", Y: " + positionY);
                     break;
                 case 6:
                     canvasName = "overlayCanvas6"
                     clearCanvas(canvasName);
-                    drawTriangle(canvasName, positionX, positionY, 4, angle, 'blue');
+                    //drawTriangle(canvasName, positionX, positionY, 8, angle, 'red'); // 'blue'
+                    drawShip(canvasName, positionX, positionY, 8, angle, 'red'); // 'blue'
+                    drawTriangle(canvasName, ( 0 + 60+4) * 3.6 , ( 0 + 507) * 3.6 , 12,    1, 'white'); // pozycja 0 x 0
                     console.log("Drawing model with ID: " + modelId + " at position X: " + positionX + ", Y: " + positionY);
                     break;
                 default:
@@ -110,14 +121,15 @@ window.onload = function () {
 //      x = 430 + 1570 = 2000   ||  y = 60 + 1040 = 1100
         const bgY = backgroundCanvas.height;
         const bgX = backgroundCanvas.width;
-        const staticShift = 180;
-        scaleX = bgX/1100;
-        scaleY = bgY/2000;
+        const staticShift_y = 505+2 /*430 + 112*/;  // 505 for /1667
+        const staticShift_x = 60 +3*1;//60 + 15;    // 60+3 for /1110
+        scaleX = bgX/1110;//1200;
+        scaleY = bgY/1667;//1800;
         console.log("ScaleX: " + scaleX + ", ScaleY: " + scaleY);
         console.log("Old X: " + oldX + ", Old Y:  " + oldY)
         // Changed coordinate system x->y , y->x
-        const y = (-oldX + 430 + staticShift) * scaleY;
-        const x = (oldY + 60) * scaleX;
+        const y = (-oldX /*+ 430*/ + staticShift_y) * scaleY;
+        const x = (oldY /*+ 60*/ + staticShift_x ) * scaleX;
         console.log("New X: " + x + ", New Y: " +  y);
         return {x, y};
     }
@@ -193,55 +205,57 @@ window.onload = function () {
         ctx.stroke();
     }
 
-    // Draw a new star at a random position every 1 second
-//    setInterval(() => {
-//        const point = getRandomPoint('overlayCanvas')
-//        const randomAngle = getRandomAngle();
-//        textField.innerText = 'Angle= ' + randomAngle.toFixed(1) + '°' + ', X=' + point.x.toFixed(2) + ', Y=' + point.y.toFixed(2);
-//        console.log('Random angle:', randomAngle);
-//        console.log('Random point:', point);
-//        clearCanvas('overlayCanvas')
-//        drawTriangle('overlayCanvas', point.x, point.y, 22, randomAngle, 'blue');
-//    }, 1000);
+// Function to draw a triangle
+    function drawShip(elementId, x, y, scale, angle, fillColor) {
+        const element = document.getElementById(elementId);
+        const ctx = element.getContext('2d');
+        // Define the vertices of the triangle (equilateral triangle centered at origin)
+        let vertices = [
+            {x: 0, y: -2},
+            {x: 0.866, y: -1.0},
+            {x: 0.866, y: 2.5},
+            {x: -0.866, y: 2.5},
+            {x: -0.866, y: -1.0}
+        ];
 
-    // Draw a new star at a random position every 1 second
-//    setInterval(() => {
-//        const point = getRandomPoint('overlayCanvas2');
-//        const randomAngle = getRandomAngle();
-//        textField.innerText = 'Angle= ' + randomAngle.toFixed(1) + '°' + ', X=' + point.x.toFixed(2) + ', Y=' + point.y.toFixed(2);
-//        console.log('Random angle:', randomAngle);
-//        console.log('Random point:', point);
-//        clearCanvas('overlayCanvas2')
-//        drawTriangle('overlayCanvas2', point.x, point.y, 18, randomAngle, 'orange');
-//    }, 1233);
+        // Scale the vertices
+        vertices = vertices.map(vertex => {
+            return {
+                x: vertex.x * scale,
+                y: vertex.y * scale
+            };
+        });
 
-//    setInterval(() => {
-//        const point = getRandomPoint('overlayCanvas1');
-//        const randomAngle = getRandomAngle();
-//        textField.innerText = 'Angle= ' + randomAngle.toFixed(1) + '°' + ', X=' + point.x.toFixed(2) + ', Y=' + point.y.toFixed(2);
-//        console.log('Random angle:', randomAngle);
-//        console.log('Random point:', point);
-//        clearCanvas('overlayCanvas1')
-//        drawTriangle('overlayCanvas1', point.x, point.y, 18, randomAngle, 'black');
-//    }, 666);
+        // Rotate the vertices
+        const radians = angle * Math.PI / 180;
+        vertices = vertices.map(vertex => {
+            return {
+                x: vertex.x * Math.cos(radians) - vertex.y * Math.sin(radians),
+                y: vertex.x * Math.sin(radians) + vertex.y * Math.cos(radians)
+            };
+        });
 
-//    setInterval(() => {
-//        const point = getRandomPoint('overlayCanvas3');
-//        const randomAngle = getRandomAngle();
-//        textField.innerText = 'Angle= ' + randomAngle.toFixed(1) + '°' + ', X=' + point.x.toFixed(2) + ', Y=' + point.y.toFixed(2);
-//        console.log('Random angle:', randomAngle);
-//        console.log('Random point:', point);
-//        clearCanvas('overlayCanvas3')
-//        drawTriangle('overlayCanvas3', point.x, point.y, 13, randomAngle, 'yellow');
-//    }, 500);
+        // Translate the vertices to the (x, y) position
+        vertices = vertices.map(vertex => {
+            return {
+                x: vertex.x + x,
+                y: vertex.y + y
+            };
+        });
 
-//    setInterval(() => {
-//        const point = getRandomPoint('overlayCanvas4');
-//        const randomAngle = getRandomAngle();
-//        textField.innerText = 'Angle= ' + randomAngle.toFixed(1) + '°' + ', X=' + point.x.toFixed(2) + ', Y=' + point.y.toFixed(2);
-//        console.log('Random angle:', randomAngle);
-//        console.log('Random point:', point);
-//        clearCanvas('overlayCanvas4')
-//        drawTriangle('overlayCanvas4', point.x, point.y, 10, randomAngle, 'green');
-//    }, 2000);
+        // Draw the triangle
+        ctx.beginPath();
+        ctx.moveTo(vertices[0].x, vertices[0].y);
+        for (let i = 1; i < vertices.length; i++) {
+            ctx.lineTo(vertices[i].x, vertices[i].y);
+        }
+        ctx.closePath();
+
+        // Fill and stroke
+        ctx.fillStyle = fillColor;
+        ctx.fill();
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 1;
+        ctx.stroke();
+    }
 };
