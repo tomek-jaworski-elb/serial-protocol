558970532ceac4ab217250b1fc2d0c00f00f96eb
Tomek Jaworski <tj.toja@gmail.com>
2024-07-06 13:22:47 +0200

    Modified tests

--- src/test/java/com/jaworski/serialprotocol/service/utils/ModelDTOTest.java
+++ src/test/java/com/jaworski/serialprotocol/service/utils/ModelDTOTest.java
@@ -2,6 +2,8 @@ package com.jaworski.serialprotocol.service.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.jaworski.serialprotocol.dto.ModelTrackDTO;
+import com.jaworski.serialprotocol.service.utils.impl.MessageCommon;
+import com.jaworski.serialprotocol.service.utils.impl.MessageLadyMarie;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,11 +16,18 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(classes = JsonMapperService.class)
+@SpringBootTest
 class ModelDTOTest {
 
     @Autowired
     private JsonMapperService jsonMapperService;
+    @Autowired(required = true)
+    private MessageTranslator messageTranslator;
+
+    @Autowired(required = true)
+    private MessageCommon messageCommon;
+    @Autowired(required = true)
+    private MessageLadyMarie messageLadyMarie;
 
 
     private static Stream<Arguments> provideInputData() {
@@ -38,7 +47,10 @@ class ModelDTOTest {
 
             Arguments.of(new byte[]{100, 51, 4, -1, 3, 4, 96, 101, 98, 3, -4, 127, 1, 104, -69, -10, 65, 88, 52, -96, 66, 127, 0, 9, -105, 13, 10}, 3),
             Arguments.of(new byte[]{100, 51, 5, 1, 3, 4, 96, 101, 98, 3, -4, 127, 2, -74, -77, -10, 65, 122, 51, -96, 66, -128, 0, 7, 1, 13, 10}, 3),
-            Arguments.of(new byte[]{100, 51, 5, 1, 3, 4, 97, 101, 97, 3, -4, 105, 1, -6, -52, -10, 65, 15, 51, -96, 66, -128, 0, 9, -34, 13, 10}, 3)
+            Arguments.of(new byte[]{100, 51, 5, 1, 3, 4, 97, 101, 97, 3, -4, 105, 1, -6, -52, -10, 65, 15, 51, -96, 66, -128, 0, 9, -34, 13, 10}, 3),
+
+            // Lady Marie
+            Arguments.of(new byte[]{108, 54, 8, -96, 0, 0, 100, 100, 100, -73, 97, 0, 0, 124, 2, 23, -44, 95, 66, 57, -39, 126, 66, -127, 0, -97, -118, 13, 10}, 6)
     );
   }
 
@@ -46,7 +58,7 @@ class ModelDTOTest {
   @MethodSource("provideInputData")
   @DisplayName("Test getModelId with various inputs")
   void realDataMessageToDTOTest(byte[] message, int expected) throws JsonProcessingException {
-    ModelTrackDTO dto = MessageTranslator.getDTO(message);
+    ModelTrackDTO dto = messageTranslator.getDTO(message);
     System.out.println(dto);
     System.out.println(jsonMapperService.toJsonString(dto));
     System.out.println("---------");
@@ -57,17 +69,37 @@ class ModelDTOTest {
   @Test
   void messageToShortTest() {
     byte[] bytes = {100, 51, 5, 1, 3, 4, 97, 101, 97, 3, -4, 105, 1, -6, -52, -10, 65, 15, 51, -96, 0, 9, -34, 13, 10};
-    assertThrows(IllegalArgumentException.class, () -> MessageTranslator.getDTO(bytes));
+    assertThrows(IllegalArgumentException.class, () -> messageTranslator.getDTO(bytes));
   }
 
   @Test
   void messageToLongTest() {
     byte[] bytes = {100, 51, 5, 1, 3, 4, 97, 101, 97, 3, -4, 105, 1, -6, -52, -10, 65, 15, 51, -96, 0, 9, -34, 13, 10, 10};
-    assertThrows(IllegalArgumentException.class, () -> MessageTranslator.getDTO(bytes));
+    assertThrows(IllegalArgumentException.class, () -> messageTranslator.getDTO(bytes));
   }
 
   @Test
   void messageNullTest() {
-    assertThrows(IllegalArgumentException.class, () -> MessageTranslator.getDTO(null));
+    assertThrows(IllegalArgumentException.class, () -> messageTranslator.getDTO(null));
+  }
+
+  @Test
+  void messageEmptyTest() {
+    assertThrows(IllegalArgumentException.class, () -> messageTranslator.getDTO(new byte[0]));
+  }
+
+  @Test
+  void ladyMarieMessageToDTOTest() {
+    byte[] bytes = {108, 54, 8, -96, 0, 0, 100, 100, 100, -73, 97, 0, 0, 124, 2, 23, -44, 95, 66, 57, -39, 126, 66, -127, 0, -97, -118, 13, 10};
+    ModelTrackDTO dto = messageTranslator.getDTO(bytes);
+    assertNotNull(dto);
+    assertEquals(6, dto.getModelName());
+  }
+
+  @Test
+  void commonMessageTest() {
+    byte[] bytes = {119, 49, 8, -69, 8, -69, 0, 6, 0, 8, -69, -123, 1, 50, 73, -114, 66, 4, 27, 111, 66, 111, 0, 3, 10, 13, 10};
+      ModelTrackDTO dto = messageTranslator.getDTO(bytes);
+      assertNotNull(dto);
   }
 }
