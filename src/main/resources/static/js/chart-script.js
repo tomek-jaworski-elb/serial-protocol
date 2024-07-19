cc3c943a0fe0c35aa7271cf89b855c517ca96cb2
JanuszW <januszw@onet.pl>
2024-07-19 09:06:32 +0200

    modele skalowane, pozycja anteny GPS v0.1

--- src/main/resources/static/js/chart-script.js
+++ src/main/resources/static/js/chart-script.js
@@ -59,41 +59,6 @@ window.onpageshow = function () {
 //                  drawTriangle(canvasName, (570  + 64) * 3.61 , ( -362 + 506) * 3.61 , 6,    1, 'red');               // boja kompielisko         320x570
 //                  drawTriangle(canvasName, (820  + 64) * 3.61 , (  610 + 506) * 3.61 , 6,    1, 'red');               // -> zatoka               -610x820
 //                  drawTriangle(canvasName, (926  + 64) * 3.61 , ( 1149 + 506) * 3.61 , 6,    1, 'red');               // Wiata END jeziora      -1149x926
-/*
-Linux -> Futro 720 :
-serial-ports-server  | 18-07-2024 14:11:06.876 [scheduling-1   ] INFO  c.j.s.s.utils.impl.SerialPortChecker.checkPorts - Checking all open ports health...
-serial-ports-server  | 18-07-2024 14:11:06.940 [main           ] INFO  c.j.s.s.controller.SerialController.lambda$openAllPorts$1 - Found 1 ports: (Physical Port S0)
-serial-ports-server  | 18-07-2024 14:11:06.953 [main           ] INFO  c.j.s.s.l.SerialPortListenerImpl.getMessageDelimiter - Set message delimiter: [13, 10]
-serial-ports-server  | 18-07-2024 14:11:06.960 [main           ] INFO  c.j.s.s.controller.SerialController.openAllPorts - On port Physical Port S0 with baud rate 9600 added listener: true
-
-Windows10 -> PC_FBZ :
-18-07-2024 16:14:13.052 [scheduling-1   ] INFO  c.j.s.s.utils.impl.SerialPortChecker.checkPorts - Checking all open ports health...
-18-07-2024 16:14:13.113 [restartedMain  ] INFO  c.j.s.s.controller.SerialController.lambda$openAllPorts$1 - Found 11 ports: (Multifunction Device,Multifunction Device,Multifunction Device,Multifunction Device,Multifunction Device,Multifunction Device,Port komunikacyjny (COM1),com0com - serial port emulator,com0com - serial port emulator,com0com - serial port emulator,com0com - serial port emulator)
-18-07-2024 16:14:13.151 [restartedMain  ] INFO  c.j.s.s.l.SerialPortListenerImpl.getMessageDelimiter - Set message delimiter: [13, 10]
-18-07-2024 16:14:13.153 [restartedMain  ] INFO  c.j.s.s.controller.SerialController.openAllPorts - On port Multifunction Device with baud rate 9600 added listener: true
-18-07-2024 16:14:13.155 [restartedMain  ] INFO  c.j.s.s.l.SerialPortListenerImpl.getMessageDelimiter - Set message delimiter: [13, 10]
-18-07-2024 16:14:13.156 [restartedMain  ] INFO  c.j.s.s.controller.SerialController.openAllPorts - On port Multifunction Device with baud rate 9600 added listener: true
-18-07-2024 16:14:13.159 [restartedMain  ] INFO  c.j.s.s.l.SerialPortListenerImpl.getMessageDelimiter - Set message delimiter: [13, 10]
-18-07-2024 16:14:13.159 [restartedMain  ] INFO  c.j.s.s.controller.SerialController.openAllPorts - On port Multifunction Device with baud rate 9600 added listener: true
-18-07-2024 16:14:13.163 [restartedMain  ] INFO  c.j.s.s.l.SerialPortListenerImpl.getMessageDelimiter - Set message delimiter: [13, 10]
-18-07-2024 16:14:13.163 [restartedMain  ] INFO  c.j.s.s.controller.SerialController.openAllPorts - On port Multifunction Device with baud rate 9600 added listener: true
-18-07-2024 16:14:13.166 [restartedMain  ] INFO  c.j.s.s.l.SerialPortListenerImpl.getMessageDelimiter - Set message delimiter: [13, 10]
-18-07-2024 16:14:13.167 [restartedMain  ] INFO  c.j.s.s.controller.SerialController.openAllPorts - On port Multifunction Device with baud rate 9600 added listener: true
-18-07-2024 16:14:13.171 [restartedMain  ] INFO  c.j.s.s.l.SerialPortListenerImpl.getMessageDelimiter - Set message delimiter: [13, 10]
-18-07-2024 16:14:13.171 [restartedMain  ] INFO  c.j.s.s.controller.SerialController.openAllPorts - On port Multifunction Device with baud rate 9600 added listener: true
-18-07-2024 16:14:13.173 [restartedMain  ] INFO  c.j.s.s.l.SerialPortListenerImpl.getMessageDelimiter - Set message delimiter: [13, 10]
-18-07-2024 16:14:13.174 [restartedMain  ] INFO  c.j.s.s.controller.SerialController.openAllPorts - On port Port komunikacyjny (COM1) with baud rate 9600 added listener: true
-18-07-2024 16:14:13.176 [restartedMain  ] INFO  c.j.s.s.l.SerialPortListenerImpl.getMessageDelimiter - Set message delimiter: [13, 10]
-18-07-2024 16:14:13.176 [restartedMain  ] INFO  c.j.s.s.controller.SerialController.openAllPorts - On port com0com - serial port emulator with baud rate 9600 added listener: true
-18-07-2024 16:14:13.178 [restartedMain  ] INFO  c.j.s.s.l.SerialPortListenerImpl.getMessageDelimiter - Set message delimiter: [13, 10]
-18-07-2024 16:14:13.178 [restartedMain  ] INFO  c.j.s.s.controller.SerialController.openAllPorts - On port com0com - serial port emulator with baud rate 9600 added listener: true
-18-07-2024 16:14:13.181 [restartedMain  ] INFO  c.j.s.s.l.SerialPortListenerImpl.getMessageDelimiter - Set message delimiter: [13, 10]
-18-07-2024 16:14:13.181 [restartedMain  ] INFO  c.j.s.s.controller.SerialController.openAllPorts - On port com0com - serial port emulator with baud rate 9600 added listener: true
-18-07-2024 16:14:13.186 [restartedMain  ] INFO  c.j.s.s.l.SerialPortListenerImpl.getMessageDelimiter - Set message delimiter: [13, 10]
-18-07-2024 16:14:13.186 [restartedMain  ] INFO  c.j.s.s.controller.SerialController.openAllPorts - On port com0com - serial port emulator with baud rate 9600 added listener: true
-18-07-2024 16:14:13.197 [Thread-14      ] INFO  c.j.s.s.l.SerialPortListenerImpl.serialEvent - On port Port komunikacyjny (COM1) Received delimited message: [16, -120, -115, 108, -12, -74, -126, 19, -119, -56, 37, 65, 111, 0, 4, -83, 13, 10]
-*/
-
                     console.log("Drawing model with ID: " + modelId + " at position X: " + positionX + ", Y: " + positionY);
                     break;
                 case 2:
