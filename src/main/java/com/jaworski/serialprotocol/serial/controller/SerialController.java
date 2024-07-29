9581645348f9dec1f9c620536639b6aed24dae63
Tomasz Jaworski <tomasz.jaworski@adtran.com>
2024-07-29 07:38:30 +0200

    Added service to serial port

--- src/main/java/com/jaworski/serialprotocol/serial/controller/SerialController.java
+++ src/main/java/com/jaworski/serialprotocol/serial/controller/SerialController.java
@@ -1,17 +1,16 @@
 package com.jaworski.serialprotocol.serial.controller;
 
 import com.fazecast.jSerialComm.SerialPort;
-import com.fazecast.jSerialComm.SerialPortDataListener;
-import com.fazecast.jSerialComm.SerialPortEvent;
 import com.jaworski.serialprotocol.resources.Resources;
 import com.jaworski.serialprotocol.serial.listener.SerialPortListenerImpl;
+import com.jaworski.serialprotocol.service.SerialPortService;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 @RequiredArgsConstructor
 @Component
@@ -19,60 +18,34 @@ public class SerialController {
     private static final Logger LOG = LogManager.getLogger(SerialController.class);
     private final SerialPortListenerImpl serialPortDataListener;
     private final Resources resources;
-
-    public void run() {
-        SerialPort[] commPorts = SerialPort.getCommPorts();
-        LOG.info("Ports count: {}",commPorts.length);
-        SerialPort port = SerialPort.getCommPorts()[0];
-        port.setBaudRate(resources.getBaudRate());
-        port.openPort(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
-    }
-
-
-    public void list() {
-        SerialPort port = SerialPort.getCommPorts()[0];
-        port.addDataListener(serialPortDataListener);
-    }
-
-    public void listener() {
-        SerialPort port = SerialPort.getCommPorts()[0];
-        port.addDataListener(new SerialPortDataListener() {
-            @Override
-            public int getListeningEvents() {
-                return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
-            }
-
-            @Override
-            public void serialEvent(SerialPortEvent event) {
-                if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
-                    return;
-                byte[] newData = new byte[port.bytesAvailable()];
-                int numRead = port.readBytes(newData, newData.length);
-                LOG.info("On port {} Read {} bytes:{}{}", port.getPortDescription(), numRead, System.lineSeparator(), newData);
-            }
-        });
-    }
+    private final SerialPortService serialPortService;
 
     public void openAllPorts() {
+        LOG.info("Opening all ports");
         LOG.debug("SerialPort version: {}", SerialPort.getVersion());
-        SerialPort[] commPorts = SerialPort.getCommPorts();
-        if (commPorts.length == 0) {
+        LOG.info("Found ports: {}",  getAllPorts().size());
+        if (serialPortService.getSerialPorts().isEmpty()) {
             LOG.info("No ports found.");
         } else {
-            Arrays.stream(commPorts)
-                    .map(SerialPort::getPortDescription)
-                    .reduce((serialPort, serialPort2) -> serialPort + "," + serialPort2)
-                    .ifPresent(message -> LOG.info("Found {} ports: ({})", commPorts.length, message));
-            for (SerialPort port : commPorts) {
-                port.openPort(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
-                boolean added = port.addDataListener(serialPortDataListener);
-                LOG.info("On port {} with baud rate {} added listener: {}", port.getPortDescription(), port.getBaudRate(), added);
-            }
+            serialPortService.getSerialPorts().forEach(getSerialPortConsumer());
         }
     }
 
+    private Consumer<SerialPort> getSerialPortConsumer() {
+        return port -> {
+            boolean listener = port.addDataListener(serialPortDataListener);
+            port.openPort(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+            LOG.info("On port {} with baud rate {} added listener: {}", port.getPortDescription(), port.getBaudRate(), listener);
+        };
+    }
+
     public List<SerialPort> getAllPorts() {
-        return Arrays.stream(SerialPort.getCommPorts())
-                .toList();
+        return serialPortService.getSerialPorts();
+    }
+
+    public void closeAllPorts() {
+        serialPortService.getSerialPorts().stream()
+                .filter(SerialPort::isOpen)
+                .forEach(SerialPort::closePort);
     }
 }
--- src/main/java/com/jaworski/serialprotocol/service/utils/impl/SerialPortChecker.java
+++ src/main/java/com/jaworski/serialprotocol/service/utils/impl/SerialPortChecker.java
@@ -1,7 +1,7 @@
 package com.jaworski.serialprotocol.service.utils.impl;
 
 import com.fazecast.jSerialComm.SerialPort;
-import com.jaworski.serialprotocol.serial.controller.SerialController;
+import com.jaworski.serialprotocol.service.SerialPortService;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,7 +15,7 @@ import java.util.concurrent.TimeUnit;
 public class SerialPortChecker {
 
   private static final Logger LOG = LogManager.getLogger(SerialPortChecker.class);
-  private final SerialController serialController;
+  private final SerialPortService serialController;
 
   public void isPortAvailable(SerialPort serialPort) {
     int lastErrorCode = serialPort.getLastErrorCode();
@@ -29,6 +29,6 @@ public class SerialPortChecker {
   @Scheduled(fixedRate = 10, timeUnit = TimeUnit.SECONDS)
   public void checkPorts() {
     LOG.info("Checking all open ports health...");
-    serialController.getAllPorts().forEach(this::isPortAvailable);
+    serialController.getSerialPorts().forEach(this::isPortAvailable);
   }
 }
