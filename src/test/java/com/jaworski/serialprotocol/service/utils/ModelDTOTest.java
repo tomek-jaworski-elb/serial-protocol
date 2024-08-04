32bcdf2421e3be47d04e60bba3df3c3cb1647bb1
Tomek Jaworski <tj.toja@gmail.com>
2024-08-04 21:18:06 +0200

    modified test

--- src/test/java/com/jaworski/serialprotocol/service/utils/ModelDTOTest.java
+++ src/test/java/com/jaworski/serialprotocol/service/utils/ModelDTOTest.java
@@ -12,9 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ModelDTOTest {
@@ -26,36 +24,36 @@ class ModelDTOTest {
 
     private static Stream<Arguments> provideInputData() {
     return Stream.of(
-            Arguments.of(new byte[]{119, 49, 8, -69, 8, -69, 0, 6, 0, 8, -69, -123, 1, 50, 73, -114, 66, 4, 27, 111, 66, 111, 0, 3, 10, 13, 10}, 1),
-            Arguments.of(new byte[]{119, 49, 8, -69, 8, -69, 0, 6, 0, 8, -69, -123, 1, 50, 73, -114, 66, 4, 27, 111, 66, 111, 0, 3, 10, 13, 10}, 1),
-            Arguments.of(new byte[]{119, 49, 8, -69, 8, -69, 0, 6, 0, 8, -69, -123, 0, -57, 77, -114, 66, -33, 33, 111, 66, 111, 0, 3, -125, 13, 10}, 1),
-            Arguments.of(new byte[]{119, 49, 8, -69, 8, -69, 0, 6, 0, 8, -69, -123, 0, 62, 73, -114, 66, 60, 32, 111, 66, 111, 0, 2, 81, 13, 10}, 1),
+            Arguments.of(new byte[]{119, 49, 8, -69, 8, -69, 0, 6, 0, 8, -69, -123, 1, 50, 73, -114, 66, 4, 27, 111, 66, 111, 0, 3, 10, 13, 10}, 1, null),
+            Arguments.of(new byte[]{119, 49, 8, -69, 8, -69, 0, 6, 0, 8, -69, -123, 1, 50, 73, -114, 66, 4, 27, 111, 66, 111, 0, 3, 10, 13, 10}, 1, null),
+            Arguments.of(new byte[]{119, 49, 8, -69, 8, -69, 0, 6, 0, 8, -69, -123, 0, -57, 77, -114, 66, -33, 33, 111, 66, 111, 0, 3, -125, 13, 10}, 1, null),
+            Arguments.of(new byte[]{119, 49, 8, -69, 8, -69, 0, 6, 0, 8, -69, -123, 0, 62, 73, -114, 66, 60, 32, 111, 66, 111, 0, 2, 81, 13, 10}, 1, null),
 
-            Arguments.of(new byte[]{98, 50, 5, 76, 5, 76, 3, -97, 3, 5, 76, 127, 10, 2, 77, 42, -61, 65, -104, -82, 67, -125, 0, 3, 65, 13, 10}, 2),
-            Arguments.of(new byte[]{98, 50, 5, 70, 5, 70, 3, -98, 3, 5, 70, -126, 9, 69, 28, 42, -61, -102, 106, -82, 67, -125, 0, 5, 111, 13, 10}, 2),
-            Arguments.of(new byte[]{98, 50, 5, 80, 5, 80, 3, -97, 3, 5, 80, -126, 10, 120, 108, 42, -61, 73, -81, -82, 67, -125, 0, 4, 5, 13, 10}, 2),
+            Arguments.of(new byte[]{98, 50, 5, 76, 5, 76, 3, -97, 3, 5, 76, 127, 10, 2, 77, 42, -61, 65, -104, -82, 67, -125, 0, 3, 65, 13, 10}, 2, null),
+            Arguments.of(new byte[]{98, 50, 5, 70, 5, 70, 3, -98, 3, 5, 70, -126, 9, 69, 28, 42, -61, -102, 106, -82, 67, -125, 0, 5, 111, 13, 10}, 2, null),
+            Arguments.of(new byte[]{98, 50, 5, 80, 5, 80, 3, -97, 3, 5, 80, -126, 10, 120, 108, 42, -61, 73, -81, -82, 67, -125, 0, 4, 5, 13, 10}, 2, null),
 
-            Arguments.of(new byte[]{98, 50, 12, 47, 12, 47, 3, 100, 3, 12, 47, -126, 0, 68, 35, -21, 65, -67, 44, -104, 66, 127, 0, 4, 10, 13, 10}, 2),
-            Arguments.of(new byte[]{98, 50, 12, 47, 12, 47, 3, 100, 3, 12, 47, -126, 1, -59, 27, -21, 65, 123, 46, -104, 66, 127, 0, 4, 68, 13, 10}, 2),
-            Arguments.of(new byte[]{98, 50, 12, 47, 12, 47, 3, 100, 3, 12, 47, -126, 2, -7, 20, -21, 65, 126, 41, -104, 66, 127, 0, 4, 112, 13, 10}, 2),
+            Arguments.of(new byte[]{98, 50, 12, 47, 12, 47, 3, 100, 3, 12, 47, -126, 0, 68, 35, -21, 65, -67, 44, -104, 66, 127, 0, 4, 10, 13, 10}, 2, null),
+            Arguments.of(new byte[]{98, 50, 12, 47, 12, 47, 3, 100, 3, 12, 47, -126, 1, -59, 27, -21, 65, 123, 46, -104, 66, 127, 0, 4, 68, 13, 10}, 2, null),
+            Arguments.of(new byte[]{98, 50, 12, 47, 12, 47, 3, 100, 3, 12, 47, -126, 2, -7, 20, -21, 65, 126, 41, -104, 66, 127, 0, 4, 112, 13, 10}, 2, null),
 
-            Arguments.of(new byte[]{100, 51, 4, -1, 3, 4, 96, 101, 98, 3, -4, 127, 1, 104, -69, -10, 65, 88, 52, -96, 66, 127, 0, 9, -105, 13, 10}, 3),
-            Arguments.of(new byte[]{100, 51, 5, 1, 3, 4, 96, 101, 98, 3, -4, 127, 2, -74, -77, -10, 65, 122, 51, -96, 66, -128, 0, 7, 1, 13, 10}, 3),
-            Arguments.of(new byte[]{100, 51, 5, 1, 3, 4, 97, 101, 97, 3, -4, 105, 1, -6, -52, -10, 65, 15, 51, -96, 66, -128, 0, 9, -34, 13, 10}, 3),
+            Arguments.of(new byte[]{100, 51, 4, -1, 3, 4, 96, 101, 98, 3, -4, 127, 1, 104, -69, -10, 65, 88, 52, -96, 66, 127, 0, 9, -105, 13, 10}, 3, null),
+            Arguments.of(new byte[]{100, 51, 5, 1, 3, 4, 96, 101, 98, 3, -4, 127, 2, -74, -77, -10, 65, 122, 51, -96, 66, -128, 0, 7, 1, 13, 10}, 3, null),
+            Arguments.of(new byte[]{100, 51, 5, 1, 3, 4, 97, 101, 97, 3, -4, 105, 1, -6, -52, -10, 65, 15, 51, -96, 66, -128, 0, 9, -34, 13, 10}, 3, null),
 
             // Lady Marie
-            Arguments.of(new byte[]{108, 54, 8, -96, 0, 0, 100, 100, 100, -73, 97, 0, 0, 124, 2, 23, -44, 95, 66, 57, -39, 126, 66, -127, 0, -97, -118, 13, 10}, 6),
+            Arguments.of(new byte[]{108, 54, 8, -96, 0, 0, 100, 100, 100, -73, 97, 0, 0, 124, 2, 23, -44, 95, 66, 57, -39, 126, 66, -127, 0, -97, -118, 13, 10}, 6, null),
 
             // Override message
-            Arguments.of(new byte[]{0, 0, 100, 100, 100, -73, 97, 0, 0, 124, 3, 25, 0, 96, 66, -50, 111, -2, 87, 85, 85, 85, 85, 85, 5, -64, 53, 124, 98, 50, 8, -67, 8, -67, 0, 100, 0, 8, -67, -126, 1, -43, 36, 124, 66, -19, 24, 126, 66, 127, 0, 2, -57, 13, 10}, 2)
+            Arguments.of(new byte[]{0, 0, 100, 100, 100, -73, 97, 0, 0, 124, 3, 25, 0, 96, 66, -50, 111, -2, 87, 85, 85, 85, 85, 85, 5, -64, 53, 124, 98, 50, 8, -67, 8, -67, 0, 100, 0, 8, -67, -126, 1, -43, 36, 124, 66, -19, 24, 126, 66, 127, 0, 2, -57, 13, 10}, 2, null)
     );
   }
 
   @ParameterizedTest
   @MethodSource("provideInputData")
   @DisplayName("Test getModelId with various inputs")
-  void realDataMessageToDTOTest(byte[] message, int expected) throws JsonProcessingException {
-    ModelTrackDTO dto = messageTranslator.getDTO(message);
+  void realDataMessageToDTOTest(byte[] message, int expected,  Class<?> expectedException) throws JsonProcessingException, IllegalArgumentException {
+    ModelTrackDTO dto = assertDoesNotThrow(() -> messageTranslator.getDTO(message));
     System.out.println(dto);
     System.out.println(jsonMapperService.toJsonString(dto));
     System.out.println("---------");
