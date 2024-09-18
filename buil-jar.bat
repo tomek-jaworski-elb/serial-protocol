14efa8815a0eaaf9cbd03005437799529dd8826b
Tomek Jaworski <tj.toja@gmail.com>
2024-09-18 21:22:09 +0200

    added batch scripts

---
+++ buil-jar.bat
@@ -0,0 +1,4 @@
+@echo off
+REM Batch Script to Check Java Version and Run the db-access-service
+
+mvnw clean package -Dmaven.test.skip=true
---
+++ run.bat
@@ -0,0 +1,35 @@
+@echo off
+REM Batch Script to Check Java Version and Run the db-access-service
+
+SET JAR_FILE_VERSION=1.5
+SET JAVA_TOOL_OPTIONS=-Xms256m -Xmx512m
+SET APP_FILE=serial-protocol
+
+echo =====================================
+echo Checking for Java installation ...
+echo =====================================
+java -version
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo Error: Java is not installed or not configured correctly.
+    echo Please install Java or check your environment variables.
+    pause
+    exit /b 1
+)
+
+echo =====================================
+echo Running %APP_FILE%-%JAR_FILE_VERSION% ...
+echo =====================================
+java -jar target/%APP_FILE%-%JAR_FILE_VERSION%.jar %JAVA_TOOL_OPTIONS%
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo Error: Failed to start %APP_FILE%.
+    echo Please check the jar file and try again.
+    pause
+    exit /b 1
+)
+
+echo =====================================
+echo %APP_FILE% is running successfully.
+echo =====================================
+pause
