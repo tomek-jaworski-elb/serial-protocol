0b27a78431631e6d70dfd78be06353c3bbfe4c04
Tomek Jaworski <tj.toja@gmail.com>
2024-10-08 20:05:26 +0200

    fixed regex patterns

--- src/test/java/com/jaworski/serialprotocol/service/tracks/TrackServiceTest.java
+++ src/test/java/com/jaworski/serialprotocol/service/tracks/TrackServiceTest.java
@@ -37,7 +37,7 @@ class TrackServiceTest {
 
     @Test
     void readModel6() throws IOException {
-        when(readLogsFileService.readLogs()).thenReturn(List.of("19-09-2024 17:24:45.570 - Translated message: ModelTrackDTO(modelName=6, positionX=55.96, positionY=63.71, speed=0.0, heading=229.8, rudder=0.0, gpsQuality=1.59, engine=100.0, bowThruster=-127.0, bowTug=TugDTO(tugForce=10.0, tugDirection=0.0), sternTug=TugDTO(tugForce=10.0, tugDirection=469.45))\n"));
+        when(readLogsFileService.readLogs()).thenReturn(List.of("19-09-2024 17:24:45.570 - Translated message: ModelTrackDTO(modelName=6, positionX=-155.96, positionY=63.71, speed=0.0, heading=229.8, rudder=0.0, gpsQuality=1.59, engine=100.0, bowThruster=-127.0, bowTug=TugDTO(tugForce=10.0, tugDirection=0.0), sternTug=TugDTO(tugForce=10.0, tugDirection=469.45))\n"));
         List<LogItem> modelTrackDTOS = trackService.readModel();
         assertNotNull(modelTrackDTOS);
         assertFalse(modelTrackDTOS.isEmpty());
@@ -45,7 +45,7 @@ class TrackServiceTest {
         LogItem modelTrackDTO = modelTrackDTOS.get(0);
         assertNotNull(modelTrackDTO);
         assertEquals(6, modelTrackDTO.getModelTrack().getModelName());
-        assertEquals(55.96, modelTrackDTO.getModelTrack().getPositionX(), 0.01);
+        assertEquals(-155.96, modelTrackDTO.getModelTrack().getPositionX(), 0.01);
         assertEquals(63.71, modelTrackDTO.getModelTrack().getPositionY(), 0.01);
         assertEquals(0.0, modelTrackDTO.getModelTrack().getSpeed(), 0.01);
         assertEquals(229.8, modelTrackDTO.getModelTrack().getHeading(), 0.01);
@@ -65,7 +65,7 @@ class TrackServiceTest {
 
     @Test
     void readModel2() throws IOException {
-        when(readLogsFileService.readLogs()).thenReturn(List.of("21-09-2024 18:43:58.682 - Translated message: ModelTrackDTO(modelName=2, positionX=29.39, positionY=76.08, speed=0.2, heading=320.9, rudder=-12.6, gpsQuality=0.04, engine=100.0, bowThruster=127.0, bowTug=TugDTO(tugForce=0.3, tugDirection=31.19), sternTug=TugDTO(tugForce=0.3, tugDirection=31.19))\n"));
+        when(readLogsFileService.readLogs()).thenReturn(List.of("21-09-2024 18:43:58.682 - Translated message: ModelTrackDTO(modelName=2, positionX=29.39, positionY=-76.08, speed=0.2, heading=320.9, rudder=-12.6, gpsQuality=0.04, engine=100.0, bowThruster=127.0, bowTug=TugDTO(tugForce=0.3, tugDirection=31.19), sternTug=TugDTO(tugForce=0.3, tugDirection=31.19))\n"));
         List<LogItem> modelTrackDTOS = trackService.readModel();
         assertNotNull(modelTrackDTOS);
         assertFalse(modelTrackDTOS.isEmpty());
@@ -74,7 +74,7 @@ class TrackServiceTest {
         assertNotNull(modelTrackDTO);
         assertEquals(2, modelTrackDTO.getModelTrack().getModelName());
         assertEquals(29.39, modelTrackDTO.getModelTrack().getPositionX(), 0.01);
-        assertEquals(76.08, modelTrackDTO.getModelTrack().getPositionY(), 0.01);
+        assertEquals(-76.08, modelTrackDTO.getModelTrack().getPositionY(), 0.01);
         assertEquals(0.2, modelTrackDTO.getModelTrack().getSpeed(), 0.01);
         assertEquals(320.9, modelTrackDTO.getModelTrack().getHeading(), 0.01);
         assertEquals(-12.6, modelTrackDTO.getModelTrack().getRudder());
@@ -87,7 +87,7 @@ class TrackServiceTest {
 
     @Test
     void readModel1() throws IOException {
-        when(readLogsFileService.readLogs()).thenReturn(List.of("19-09-2024 14:10:37.358 - Translated message: ModelTrackDTO(modelName=1, positionX=71.14, positionY=59.78, speed=0.1, heading=232.5, rudder=-12.3, gpsQuality=0.03, engine=6.0, bowThruster=111.0, bowTug=TugDTO(tugForce=0.0, tugDirection=22.35), sternTug=TugDTO(tugForce=0.0, tugDirection=22.35))\n"));
+        when(readLogsFileService.readLogs()).thenReturn(List.of("19-09-2024 14:10:37.358 - Translated message: ModelTrackDTO(modelName=1, positionX=-171.14, positionY=59.78, speed=0.1, heading=232.5, rudder=-12.3, gpsQuality=0.03, engine=6.0, bowThruster=111.0, bowTug=TugDTO(tugForce=0.0, tugDirection=22.35), sternTug=TugDTO(tugForce=0.0, tugDirection=22.35))\n"));
         List<LogItem> modelTrackDTOS = trackService.readModel();
         assertNotNull(modelTrackDTOS);
         assertFalse(modelTrackDTOS.isEmpty());
@@ -95,7 +95,7 @@ class TrackServiceTest {
         LogItem modelTrackDTO = modelTrackDTOS.get(0);
         assertNotNull(modelTrackDTO);
         assertEquals(1, modelTrackDTO.getModelTrack().getModelName());
-        assertEquals(71.14, modelTrackDTO.getModelTrack().getPositionX(), 0.01);
+        assertEquals(-171.14, modelTrackDTO.getModelTrack().getPositionX(), 0.01);
         assertEquals(59.78, modelTrackDTO.getModelTrack().getPositionY(), 0.01);
         assertEquals(0.1, modelTrackDTO.getModelTrack().getSpeed(), 0.01);
         assertEquals(232.5, modelTrackDTO.getModelTrack().getHeading(), 0.01);
