0a9dd3c75ff1bad80243349e5c9191908e0cd03a
Tomek Jaworski <tj.toja@gmail.com>
2024-10-15 16:39:10 +0200

    cleanup code

--- src/main/resources/static/js/common-draw-ship.js
+++ src/main/resources/static/js/common-draw-ship.js
@@ -1,13 +1,12 @@
+function getScaledPointsNew(oldX, oldY) {
+    const map_scale_factor = 2.407;
+    const staticShift_y = 506;
+    const staticShift_x = 64;
+    const scaleX = map_scale_factor;
+    const scaleY = map_scale_factor;
 
-    function getScaledPointsNew(oldX, oldY) {
-        const map_scale_factor = 2.407;
-        const staticShift_y = 506;                                                                                      // 506
-        const staticShift_x = 64;                                                                                       //  64
-        const scaleX = map_scale_factor; /// 2.4; // 3.61 / 1.5 ; /// 1;// 3.61;                   // 2.407                                                                               //   3.61
-        const scaleY = map_scale_factor; /// 2.4; // 3.61 / 1.5 ; /// 1;// 3.61;
-        //   3.61
-        // Changed coordinate system x->y , y->x
-        const y = (-oldX + staticShift_y) * scaleY;
-        const x = (oldY + staticShift_x ) * scaleX;
-        return {x, y};
-    }
+    // Changed coordinate system x->y , y->x
+    const y = (-oldX + staticShift_y) * scaleY;
+    const x = (oldY + staticShift_x) * scaleX;
+    return {x, y};
+}
