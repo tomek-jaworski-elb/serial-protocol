6808ee5d99f603e6116df48aeac968e7bdb87ab4
Tomek Jaworski <tj.toja@gmail.com>
2024-10-25 15:39:19 +0200

    code cleanup

--- src/main/resources/static/js/chart-script.js
+++ src/main/resources/static/js/chart-script.js
@@ -21,22 +21,12 @@ window.onpageshow = function () {
        } else {
           elementsByTagNameElement.width = imgMap.width;
           elementsByTagNameElement.height = imgMap.height;
-          ///elementsByTagNameElement.width = container.clientWidth;
-          ///elementsByTagNameElement.height = container.clientHeight;
           elementsByTagNameElement.style.width = imgMap.width + 'px';
           elementsByTagNameElement.style.height =imgMap.height + 'px';
        }
     }
 
-//    todo - do usunięcia po testach
-// todo - potrzeba sprawdzić/dobrać nowy współczynnik skali
-//    drawShip("overlayCanvas1", 100, 100, 10, 90, 'red', 12.21, 2, 0);                         // Warta                  ///
-//    drawShip("overlayCanvas2", 200, 200, 10, 90, 'blue', 12.21, 2, 0);                        // Warta                  ///
-//                                    x,y,4,angle,'blue'
-//    drawTriangle("overlayCanvas1", 100, 100, 10, 90, 'blue');
-//    drawTriangle("overlayCanvas2", 200, 200, 10, 90, 'red');
-
-                  let mapa_x = 2.407; /// było 2.4   = kalibracja mapy
+    let mapa_x = 2.407; /// było 2.4   = kalibracja mapy
 
 //    drawTriangle("overlayCanvas2", (  0    + 60+4) * mapa_x , ( 0    + 506) * mapa_x , 6,    1, 'white');         // pozycja 0 x 0             0x0
 //    drawTriangle("overlayCanvas2", ( 77.07 + 60+4) * mapa_x , (97.25 + 506) * mapa_x , 6,    1, 'orange');        // SBM    -97.25x77.07
@@ -124,52 +114,6 @@ window.onpageshow = function () {
                   drawTriangle("overlayCanvas1", (820  + 64) * mapa_x , (  610 + 506) * mapa_x , 6,    1, 'red');               // -> zatoka               -610x820
                   drawTriangle("overlayCanvas1", (926  + 64) * mapa_x , ( 1149 + 506) * mapa_x , 6,    1, 'red');               // Wiata END jeziora      -1149x926
 
-
-
-/*
-drawShip(canvasName, 64, 506, 2, 90, 'blue', 12.21, 2, 0);                        // Warta                               ///
-
-                    drawShip(canvasName, 100, 100, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 200, 200, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 300, 300, 23, 90, 'orange', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 400, 400, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 500, 500, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 600, 600, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 700, 700, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 800, 800, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 900, 900, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 1000, 1000, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 1100, 1100, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 1200, 1200, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 1300, 1300, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 1400, 1400, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 1500, 1500, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 1600, 1600, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 1700, 1700, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 1800, 1800, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 1900, 1900, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 2000, 2000, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 2100, 2100, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 2200, 2200, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 2300, 2300, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 2400, 2400, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 2500, 2500, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 2600, 2600, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 2700, 2700, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 2800, 2800, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 2900, 2900, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 3000, 3000, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 3100, 3100, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 3200, 3200, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 3300, 3300, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 3400, 3400, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 3500, 3500, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 3600, 3600, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 3700, 3700, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 3800, 3800, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 3900, 3900, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                    drawShip(canvasName, 4000, 4000, 23, 90, 'red', 12.21, 2, 0);                        // Warta
-                                                                                                                            */ ///
                     console.log("Drawing model with ID: " + modelId + " at position X: " + positionX + ", Y: " + positionY);
                     break;
                 case 2:
