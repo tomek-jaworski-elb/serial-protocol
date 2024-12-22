7337f753142647a4eb4000b7361e2e6fa872ba5d
Tomek <tj.toja@gmail.com>
2024-12-22 17:44:12 +0100

    bumped version to 1.6

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 REM Batch Script to Check Java Version and Run the serial-protocol application
 
-SET JAR_FILE_VERSION=1.5
+SET JAR_FILE_VERSION=1.6
 SET JAVA_TOOL_OPTIONS=-Xms256m -Xmx512m
 SET APP_FILE=serial-protocol
 
