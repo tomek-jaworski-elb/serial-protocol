708cfeb9a0705a202e12fc3e7610fe05a6499a77
Tomek <tj.toja@gmail.com>
2025-02-20 15:14:08 +0100

    Implemented user authorization

--- src/main/java/com/jaworski/serialprotocol/authorization/SecurityConfig.java
+++ src/main/java/com/jaworski/serialprotocol/authorization/SecurityConfig.java
@@ -5,11 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -17,6 +21,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final CustomEncoder customEncoder;
+    private final CustomLogoutHandler customLogoutHandler;
+    private final CustomLoginHandler customLoginHandler;
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -36,10 +42,31 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((authorizeHttpRequests) ->
                         authorizeHttpRequests
-                                .requestMatchers("/name-service").authenticated()
-                                .requestMatchers("/api/").authenticated()
+                                .requestMatchers("/name-service").hasRole("USER")
+                                .requestMatchers("/api/**").hasRole("USER")
+                                .requestMatchers("/admin/**").hasRole("ADMIN")
                                 .anyRequest().permitAll()
-                );
+                )
+                .httpBasic(withDefaults())
+                .formLogin((formLogin) ->
+                                formLogin
+                                        .usernameParameter("username")
+                                        .passwordParameter("password")
+                                        .permitAll()
+                                        .loginPage("/login")
+                                        .failureUrl("/login?error")
+                                        .loginProcessingUrl("/login")
+                                        .successHandler(customLoginHandler)
+
+                )
+                .logout((logout) ->
+                                logout.deleteCookies("JSESSIONID")
+                                        .invalidateHttpSession(true)
+                                        .permitAll()
+                                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                                        .logoutSuccessHandler(customLogoutHandler)
+                )
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 }
--- src/main/java/com/jaworski/serialprotocol/rest/RestStudent.java
+++ src/main/java/com/jaworski/serialprotocol/rest/RestStudent.java
@@ -8,26 +8,24 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@Secured("ROLE_ADMIN,ROLE_USER")
 public class RestStudent {
 
     private static final Logger LOG = LoggerFactory.getLogger(RestStudent.class);
+    public static final String STUDENT = "/student";
 
     private final StudentService studentService;
 
-    @PostMapping(path = "/student", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
+    @PostMapping(path = STUDENT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Integer> getStudent(@RequestBody(required = true) List<StudentDTO> studentDTOS) {
         LOG.info("Received dtos size {}", studentDTOS.size());
         List<Integer> ids = new ArrayList<>();
