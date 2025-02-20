b2bdb6b3448ddf7c2cd8e6578a469eee1ffaade8
Tomek <tj.toja@gmail.com>
2025-02-20 15:14:25 +0100

    Modified tests

--- src/test/java/com/jaworski/serialprotocol/controller/web/MapControllerTest.java
+++ src/test/java/com/jaworski/serialprotocol/controller/web/MapControllerTest.java
@@ -1,12 +1,17 @@
 package com.jaworski.serialprotocol.controller.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Base64;
+import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -58,15 +63,23 @@ class MapControllerTest {
 
     @Test
     void test_NameServicePostEndpoint() throws Exception {
-        this.mockMvc.perform(post("/name-service"))
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(Collections.emptyList());
+        this.mockMvc.perform(post("/api/v1/student").contentType(MediaType.APPLICATION_JSON).content(json)
+                        .header(HttpHeaders.AUTHORIZATION, getAuthorizationHeaderValue("user", "user")))
                 .andExpect(result -> assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus()))
-                .andExpect(result -> assertEquals("text/html;charset=UTF-8", result.getResponse().getHeaderValue(HttpHeaders.CONTENT_TYPE)));
+                .andExpect(result -> assertEquals("application/json", result.getResponse().getHeaderValue(HttpHeaders.CONTENT_TYPE)));
     }
 
     @Test
     void test_NameServiceGetEndpoint() throws Exception {
-        this.mockMvc.perform(get("/name-service"))
+        this.mockMvc.perform(get("/name-service")
+                        .header(HttpHeaders.AUTHORIZATION, getAuthorizationHeaderValue("user", "user")))
                 .andExpect(result -> assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus()))
                 .andExpect(result -> assertEquals("text/html;charset=UTF-8", result.getResponse().getHeaderValue(HttpHeaders.CONTENT_TYPE)));
+    }
+
+    private String getAuthorizationHeaderValue(String username, String password) {
+        return "Basic " + Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
     }
 }
--- src/test/java/com/jaworski/serialprotocol/rest/RestStudentTest.java
+++ src/test/java/com/jaworski/serialprotocol/rest/RestStudentTest.java
@@ -7,11 +7,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.Base64;
 import java.util.Date;
 import java.util.List;
 
@@ -26,6 +28,9 @@ class RestStudentTest {
     private MockMvc mockMvc;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    private String getAuthorizationHeaderValue(String username, String password) {
+        return "Basic " + Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
+    }
 
     @Test
     void getStudent() throws Exception {
@@ -44,7 +49,7 @@ class RestStudentTest {
         List<StudentDTO> studentDTOS = List.of(studentDTO);
 
         String writeValueAsString = objectMapper.writeValueAsString(studentDTOS);
-        MvcResult mvcResult = mockMvc.perform(post("/api/v1/student")
+        MvcResult mvcResult = mockMvc.perform(post("/api/v1/student").header(HttpHeaders.AUTHORIZATION, getAuthorizationHeaderValue("user", "user"))
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(writeValueAsString)
                         .accept(MediaType.APPLICATION_JSON))
