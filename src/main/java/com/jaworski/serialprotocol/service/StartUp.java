8885442028cb5b105a138ca7fb650be068b5bfa6
Tomek Jaworski <tj.toja@gmail.com>
2026-05-11 12:34:40 +0200

    removed-http-client dependency and updated dependencies

--- src/main/java/com/jaworski/serialprotocol/service/StartUp.java
+++ src/main/java/com/jaworski/serialprotocol/service/StartUp.java
@@ -1,7 +1,5 @@
 package com.jaworski.serialprotocol.service;
 
-import com.jaworski.serialprotocol.resources.Resources;
-import com.jaworski.serialprotocol.restclient.RestNameService;
 import com.jaworski.serialprotocol.serial.controller.SerialController;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -11,13 +9,8 @@ import org.springframework.stereotype.Component;
 public class StartUp {
 
     private final SerialController serialController;
-    private final RestNameService restNameService;
-    private final Resources resources;
 
     public void start() {
         serialController.openAllPorts();
-        if (resources.isRestServiceEnabled()) {
-          restNameService.checkConnection();
-        }
     }
 }
